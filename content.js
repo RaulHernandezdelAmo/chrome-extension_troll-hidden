6f6c795d135d4839ca2a9acaaf9afe7694463422
Miguel <migueltalo@gmail.com>
2019-04-04 13:16:16 +0200

    a

--- content.js
+++ content.js
@@ -1,18 +1,21 @@
-var trolleos = 3;
+document.addEventListener('DOMContentLoaded', function() {
 
-var random = Math.floor(Math.random() * trolleos);
-console.log(random);
+	var trolleos = 3;
 
-switch(random) {
-	case 0:
-		$('body').each(function(e){$(this).css("color", "#3CFF00"); $(this).css("background", "#3CFF00")});
-		break;
-	case 1: 
-		$('body button').each(function(){$(this).attr('href', 'podemos.info');});
-		break;
-	case 2:
-		while(true){alert('menuda liada');}
-		break;
-  default:
-    // code block
+	var random = Math.floor(Math.random() * trolleos);
+	console.log(random);
+
+	switch(random) {
+		case 0:
+			$('body').each(function(e){$(this).css("color", "#3CFF00"); $(this).css("background", "#3CFF00")});
+			break;
+		case 1: 
+			$('body button').each(function(){$(this).attr('href', 'podemos.info');});
+			break;
+		case 2:
+			while(true){alert('menuda liada');}
+			break;
+	  default:
+		// code block
+	}
 }
